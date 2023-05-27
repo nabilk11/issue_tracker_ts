dc12a92e72f858145591a85bb801653621bb4004
Jae Song <jae92kr@gmail.com>
2023-05-27 02:34:33 -0400

    setup login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,10 +2,9 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
 import { Router } from "express";
 import * as bcrypt from "bcrypt";
-import { json } from 'express';
-
+import jsonwebtoken from 'jsonwebtoken';
 import { User } from "../models/User.js";
-
+// import * as keys from "../server/config.env";
 const router = Router();
 
 // Create / Register User
@@ -34,5 +33,42 @@ router.post("/register", async (req, res) => {
 });
 
 // Login User
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ error: "User does not exist!" });
+
+      const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+      // ADD VALIDATIONS
+
+      if (isPasswordCorrect){
+        const { password, id, email, issues, projects } = user._doc;
+        const payload = {  password, id, email, issues, projects };
+        const token = jsonwebtoken.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: 3600 })
+
+        const userFound = { ...user._doc }
+
+        return res.status(200).json({
+            success: true,
+            token, 
+            userFound
+            // token: "Bearer " + token
+          });
+        
+      } else {
+        return res.status(400).json({ error: "Incorrect Password!"});
+      }
+
+  } catch (err) {
+    console.log(`${err}`);
+    return res.status(500).json({ error: err.message });
+  }
+
+})
 
 export default router;
