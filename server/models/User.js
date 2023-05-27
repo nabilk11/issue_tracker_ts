3a59aa19d0d05a5fa8629293d83209e0f1b9f086
nabilk11 <khan.nabiln11.com>
2023-05-26 20:58:15 -0400

    added user model

---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Required!"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is Required!"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required!"],
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    projects: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+      },
+    ],
+    issues: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Issue",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("User", UserSchema);
