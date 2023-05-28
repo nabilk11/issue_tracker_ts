7e9aeac74931cfd42cece85f5d2619b00eb1ac13
nabilk11 <khan.nabiln11.com>
2023-05-28 03:32:04 -0400

    fixed type annotation errors in authcontext

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,5 +1,11 @@
-import React, { useState, createContext, ReactNode, FC } from 'react';
-import axios from 'axios';
+import React, {
+  useState,
+  createContext,
+  ReactNode,
+  FC,
+  useEffect,
+} from "react";
+import axios from "axios";
 
 interface AuthContextType {
   // isLoggedIn: boolean;
@@ -9,87 +15,75 @@ interface AuthContextType {
   setUser: React.Dispatch<React.SetStateAction<null>>;
   message: string; // Replace `string` with the actual type of `message`
   setMessage: React.Dispatch<React.SetStateAction<string>>;
-  value: 
+  value: null;
+  error: unknown;
 }
 
 type ContextProps = {
-    children: ReactNode;
-  };
-
+  children: ReactNode;
+};
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: FC<ContextProps> = ({ children }) => {
-    const [user, setUser] = useState(null)
-    const [message, setMessage] = useState('')
-    // const [isLoggedIn, setIsLoggedIn] = useState(null)
+  const [user, setUser] = useState(null);
+  const [message, setMessage] = useState("");
+  // const [isLoggedIn, setIsLoggedIn] = useState(null)
 
-    const isLoggedIn = async () => {
-        try {
-            const res = await axios.get("https://localhost:8000/api/user", {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${localStorage.getItem("userToken")}`
-                }
-              
-            })
-            setUser(res.data);
-            } catch (err) {
-                
-            }
+  useEffect(() => {
+    isLoggedIn();
+  }, []);
 
+  const isLoggedIn = async () => {
+    try {
+      const res = await axios.get("https://localhost:8000/api/user", {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      setUser(res.data);
+    } catch (err) {
+      //
     }
-    // LOGIN USER
-    const loginCall = async (credentials: any) => {
-        try {
-            const res = await axios.post("https://localhost:8000/api/login", {
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                email: credentials.email,
-                password: credentials.password,
-            });
-            console.log(res)
-            console.log(res.data)
-            setMessage("Success!")
+  };
+  // LOGIN USER
+  const loginCall = async (credentials: any) => {
+    try {
+      const res = await axios.post("https://localhost:8000/api/login", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        email: credentials.email,
+        password: credentials.password,
+      });
+      console.log(res);
+      console.log(res.data);
+      setMessage("Success!");
 
-            // SET TOKEN
-            localStorage.setItem("userToken", res.data.token)
-
-            
-        } catch (err: object) {
-            setMessage(err.response.data.error);
-            
-        }
+      // SET TOKEN
+      localStorage.setItem("userToken", res.data.token);
+    } catch (error: any) {
+      setMessage(error.response.data.error);
     }
-    return (
-        <AuthContext.Provider value={{  loginCall, user, setUser, message, setMessage }}>
+  };
 
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  const authContextValue: AuthContextType = {
+    // isLoggedIn: false,
+    loginCall,
+    user,
+    setUser,
+    message,
+    setMessage,
+    value: null,
+    error: Error,
+  };
 
+  return (
+    <AuthContext.Provider value={authContextValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-
-export default AuthContext
-
-
-
-
-{
-  "success": true,
-  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZCI6IiQyYiQxMCRjSm5iLnNpQU1CT080bHRqZ0gvYWpPeFQ2NktJZlg2NTREQS5ZYWcvN2c0alZ1azVCd0hOMiIsImVtYWlsIjoiamFlMTIzQHRlc3QuY29tIiwiaXNzdWVzIjpbXSwicHJvamVjdHMiOltdLCJpYXQiOjE2ODUxNzQ1MjYsImV4cCI6MTY4NTE3ODEyNn0.TfS-kOcnQ19VGCdF0YCw_No0L2KZ0caS0tcJ_eDn1DY",
-  "userFound": {
-      "_id": "647188a7b9724fbc2ff618f6",
-      "name": "Jae Song2",
-      "email": "jae123@test.com",
-      "password": "$2b$10$cJnb.siAMBOO4ltjgH/ajOxT66KIfX654DA.Yag/7g4jVuk5BwHN2",
-      "isAdmin": false,
-      "projects": [],
-      "issues": [],
-      "createdAt": "2023-05-27T04:35:51.952Z",
-      "updatedAt": "2023-05-27T04:35:51.952Z",
-      "__v": 0
-  }
-}
+export default AuthContext;
